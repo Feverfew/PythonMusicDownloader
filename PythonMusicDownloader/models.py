76d792ee5cd64fe7c678a56a2766e9d322f0f4af
Feverfew <alexlns99@yahoo.co.uk>
2015-10-18 21:36:49 +0100

    SongDownloader object created with tests

--- PythonMusicDownloader/models.py
+++ PythonMusicDownloader/models.py
@@ -1,4 +1,18 @@
-# All models of the MVC architecture go here.
+﻿# All models of the MVC architecture go here.
+import requests
+import json
 
 class SongDownloader(object):
-    pass
+    """Downloads songs from pleer.com"""
+    def __init__(self, artist, song, download_dir):
+        """
+        Args:
+            artist (str): Name of artist.
+            song (str): Name of song.
+            download_dir (str) Name of download_dir. 
+                Make sure directory is valid or an Exception
+                will occur.
+        """
+        self.artist = artist
+        self.song = song
+        self.download_dir = download_dir
--- PythonMusicDownloader/tests.py
+++ PythonMusicDownloader/tests.py
@@ -1,8 +1,23 @@
 ﻿import unittest
+from models import SongDownloader
 
-class Test_tests(unittest.TestCase):
-    def test_A(self):
-        self.fail("Not implemented")
+class SongDownloaderTestCase(unittest.TestCase):
+
+    def test_attributes_are_set_when_object_initialised(self):
+        test_data = [["Opeth", "Blackwater Park", "C:/"],
+                     ["Mono", "Are you there", "C:/Program Files"],
+                     ["Sevendust", "Denial", "C:/"]]
+        for data in test_data:
+            song_downloader = SongDownloader(data[0], data[1], data[2])
+            self.assertEquals(song_downloader.artist, data[0])
+            self.assertEquals(song_downloader.song, data[1])
+            self.assertEquals(song_downloader.download_dir, data[2])
+
+class SongTestCase(unittest.TestCase):
+
+    def test_attributes_are_set_when_object_initialised(self):
+        pass
+
 
 if __name__ == '__main__':
     unittest.main()
