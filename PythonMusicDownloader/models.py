42715c5b30ff8794f338ec22199b77b67e6976ac
Feverfew <alexlns99@yahoo.co.uk>
2015-11-02 19:16:15 +0000

    models and tests added.

--- PythonMusicDownloader/models.py
+++ PythonMusicDownloader/models.py
@@ -2,17 +2,71 @@
 import requests
 import json
 
-class SongDownloader(object):
+class TrackDownloader(object):
     """Downloads songs from pleer.com"""
-    def __init__(self, artist, song, download_dir):
+    def __init__(self, id, key, query=None, download_dir=None):
         """
-        Args:
-            artist (str): Name of artist.
-            song (str): Name of song.
-            download_dir (str) Name of download_dir. 
-                Make sure directory is valid or an Exception
-                will occur.
+        :param id: Id of Pleer API access.
+        :type id: int
+        :param key: key of Pleer API Access.
+        :type key: str
+        :param query: Query of user.
+        :type query: str
+        :param download_dir: Download directory.
+        :type download_dir: str
         """
-        self.artist = artist
-        self.song = song
+        self.API_TOKEN_URL = "http://api.pleer.com/token.php"
+        self.API_URL = "http://api.pleer.com/index.php"
+        self.id = id
+        self.key = key
+        self.query = query
         self.download_dir = download_dir
+        self.token = self.get_access_token()
+        self.tracks = []
+
+    def get_access_token(self):
+        """Get access token from pleer.com"""
+        auth = (self.id, self.key)
+        data = {"grant_type": "client_credentials"}
+        response = requests.post(self.API_TOKEN_URL, auth=auth, data=data)
+        token = response.json().get("access_token")
+        return token
+
+    def tracks_search(self, query, page=1, result_on_page=10, quality="all"):
+        """Search for tracks
+        :param query: Query of user.
+        :type query: str
+        :param page: Selected page.
+        :type page: int
+        :param result_on_page: Amount of results on a page.
+        :type result_on_page: int
+        :param quality: Quality of file desired.
+        :type quality: str
+        """
+        data = {
+            'access_token': self.token,
+            'query': query,
+            'page': page,
+            'result_on_page': result_on_page,
+            'quality': quality    
+        }
+        response = requests.post(self.API_TOKEN_URL, data=data)
+        tracks = response.json().get("tracks")
+        for track in tracks:
+            tracks.append(Track(track))
+
+
+class Track(object):
+    """Holds data of a track"""
+    def __init__(self, track):
+        """
+        :param track: Metadata of track.
+        :type track: list.
+        """
+        self.id = track['track_id']
+        self.artist = track['artist']
+        self.track = track['track']
+        self.length = track['length']
+        self.bitrate = track['bitrate']
+        self.size = track['size']
+
--- PythonMusicDownloader/tests.py
+++ PythonMusicDownloader/tests.py
@@ -1,19 +1,33 @@
 ﻿import unittest
-from models import SongDownloader
+from models import TrackDownloader
 
-class SongDownloaderTestCase(unittest.TestCase):
+class TrackDownloaderTestCase(unittest.TestCase):
 
     def test_attributes_are_set_when_object_initialised(self):
-        test_data = [["Opeth", "Blackwater Park", "C:/"],
-                     ["Mono", "Are you there", "C:/Program Files"],
-                     ["Sevendust", "Denial", "C:/"]]
+        test_data = [["313666", "Eb0iVMUcPeym8f8JEKWG", None, None],
+                     ["345635", "sdr324RFSDv£$Wr13433", "Opeth", None],
+                     ["345632", "sdFW£$FVR£E453fsfsss", "Deftones", "C:/"]
+                     ]
         for data in test_data:
-            song_downloader = SongDownloader(data[0], data[1], data[2])
-            self.assertEquals(song_downloader.artist, data[0])
-            self.assertEquals(song_downloader.song, data[1])
-            self.assertEquals(song_downloader.download_dir, data[2])
+            song_downloader = TrackDownloader(data[0], data[1], data[2], data[3])
+            self.assertEquals(song_downloader.id, data[0])
+            self.assertEquals(song_downloader.key, data[1])
+            self.assertEquals(song_downloader.query, data[2])
+            self.assertEquals(song_downloader.download_dir, data[3])
 
-class SongTestCase(unittest.TestCase):
+    def test_for_access_token(self):
+        song_downloader = TrackDownloader("313666", "Eb0iVMUcPeym8f8JEKWG")
+        self.assertTrue(song_downloader.token)
+
+    def test_for_failure_if_parameters_missing_in_intialisation(self):
+        test_data = [[None, None, None, None],
+                     ["2342333", None, "Opeth", None],
+                     [None, "scgvcdfv2CSF£$RSC", None, "C:/"]
+                     ]
+        for data in test_data:
+            self.assertRaises(TrackDownloader(data[0], data[1], data[2], data[3]))
+
+class TrackTestCase(unittest.TestCase):
 
     def test_attributes_are_set_when_object_initialised(self):
         pass
